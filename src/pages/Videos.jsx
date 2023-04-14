1ee5bf3a74f60a80b0e9fb14de4d0d891c485c37
snail15 <jungs@uchicago.edu>
2023-04-14 00:54:18 -0700

    API Client and Video Card

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,8 +1,36 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import YoutubeMagic from '../UI/YoutubeLoader';
+import VideoCard from '../components/VideoCard';
+import axios from 'axios';
+import Youtube, { search } from '../api/youtube';
+
+import { useYoutubeApi } from '../context/YoutubeApiContext';
 
 export default function Videos() {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(['videos', keyword], () => {
+    return youtube.search(keyword);
+  });
 
-  return <div>Videos {keyword ? `${keyword}` : `Hot Trends`}</div>;
+  return (
+    <>
+      <div>Videos {keyword ? `${keyword}` : `Hot Trends`}</div>
+      {isLoading && <YoutubeMagic />}
+      {error && <p>Something is wrong</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{jsxjs}'],
+  content: ['./src/**/*.{jsx,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: '#FF0000',
+      },
+    },
   },
   plugins: [],
 };
